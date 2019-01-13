8bbb7e2aa72a760010be7daecfcc611348d89c70
Hugo <hugo.bourniche@etu.univ-nantes.fr>
2019-01-13 21:56:47 +0100

    Question pour un champion : Naive Face à LaPlace Oui oui oui cette MAJ est importante, et pas irréversible mais presque j'ai amélioré le constructeur plus fait la fonction qui reconnais de qui ça vient bien qu'elle ne soit pas totalement fonctionnelle

--- src/authorReco/AuthorRecognizer1.java
+++ src/authorReco/AuthorRecognizer1.java
@@ -23,7 +23,8 @@ public class AuthorRecognizer1 extends AuthorRecognizerAbstractClass {
 	 * (e.g., "zola-bigrams"), and the values of the second map are the LanguageModel objects 
 	 * built from the file path given in the AuthorConfigurationFile attribute.
 	 */
-	protected Map<String, Map<String, LanguageModelInterface>> authorLangModelsMap;
+	private Map<String, Map<String, LanguageModelInterface>> authorLangModelsMap;
+	private Map<String, Map<String, LanguageModelInterface>> authorLangModelsMap2;
 
 	/**
 	 * Constructor.
@@ -36,40 +37,44 @@ public class AuthorRecognizer1 extends AuthorRecognizerAbstractClass {
 	 * the names of the authors recognized by the system.
 	 */
 	public AuthorRecognizer1(String configFile, String vocabFile, String authorFile) {
-	    //Initialisation du scanneur pour lire le fichier
-	    Scanner scanConfig = new Scanner(configFile);
+	    //Initialisation du fichier de configuration
+		super.loadAuthorConfigurationFile(configFile);
+		//Initialisation du scanneur pour lire le fichier
+	    List<String> scanConfig = MiscUtils.readTextFileAsStringList(configFile);
 	    //Initialisation de la HashMap qui est en attribut
-	    this.authorLangModelsMap = new HashMap();
+	    this.authorLangModelsMap = new HashMap<String, Map<String, LanguageModelInterface>>();
+		this.authorLangModelsMap2 = new HashMap<String, Map<String, LanguageModelInterface>>();
 	    //Initialisation du ngramCounts qui va permettre de créer le languagueModel qui est dans la MAP.
 	    NgramCounts ngram = new NgramCounts();
 	    //Initialisation du Vocabulary qui va de paire avec le ngram pour le languageModel. grâce à la classe supérieure
 	    super.loadVocabularyFile(vocabFile);
 	    //Initialisation du language (ici Naive mais on pourrait aussi avoir LaPlace
-        LanguageModelInterface language = new LaplaceLanguageModel();
-
+        LanguageModelInterface language = new NaiveLanguageModel();
+		LanguageModelInterface language2 = new LaplaceLanguageModel();
         //Initialisation des auteurs qui seront reconnus par le système
 		super.loadAuthorFile(authorFile);
+        //Lecture de chaque ligne du fichier
+		for(String ligne : scanConfig){
 
-        //Lire chaque ligne du fichier s'il existe.
-		while(scanConfig.hasNextLine()){
-            //Avancée dans le fichier
-            String ligne = scanConfig.nextLine();
-            //découper dans le tableau les
-            String[] contientDesMots = ligne.split(" ");
-			//Vérification que la ligne où il y a écris le nom de l'auteur coordonne avec les auteurs entrés dans le système.
-            if(super.authors.contains(contientDesMots[0]))
+            //découper dans le tableau chaque String pour avoir des mots séparés (séparation \t est la représentation de l'espace.
+            String[] contientDesMots = ligne.split("\t");
+
+            //Vérification que la ligne où il y a écris le nom de l'auteur coordonne avec les auteurs entrés dans le système.
+			if(super.authors.contains(contientDesMots[0]))
 			{
-
 				//Remplir le ngramCounts grâce au chemin qui même à l'"authorFile".
 				ngram.readNgramCountsFile(contientDesMots[2]);
 				//initialise l'intérieur du language avec le ngram et vocab initialisé juste avant
 				language.setNgramCounts(ngram, super.vocabularyLM);
+				language2.setNgramCounts(ngram, super.vocabularyLM);
 				//Créé la map qui est intégrée par la suite à la map
-				Map table = new HashMap();
+				Map<String, LanguageModelInterface> table = new HashMap();
 				//ajouter à la table, la ligne
 				table.put(contientDesMots[1], language);
 				//ajouter à l'atribut la table créé préalablement.
 				this.authorLangModelsMap.put(contientDesMots[0], table);
+				this.authorLangModelsMap2.put(contientDesMots[0], table);
+
 
 			}
 
@@ -95,11 +100,28 @@ public class AuthorRecognizer1 extends AuthorRecognizerAbstractClass {
 	 * @param sentence the sentence whose author is to recognize.
 	 * @return the author of the sentence as recognized by the recognition system.
 	 */
-	public String recognizeAuthorSentence(String sentence) {
-		/*if (sentence != " "){
-			return super.authors.get(authors.size());
-		}*/
-		return UNKNOWN_AUTHOR;
+	public String recognizeAuthorSentence(String sentence, int i) {
+		double count=0.0;
+		double tmp = 0.0;
+		String recognizeAuthor= "Je ne sais pas";
+		for(String author : super.authors)
+		{
+			if (i==0)
+			{
+				tmp = this.authorLangModelsMap.get(author).get(author+"_bi").getSentenceProb(sentence);
+			}
+			else
+			{
+				tmp = this.authorLangModelsMap2.get(author).get(author+"_bi").getSentenceProb(sentence);
+			}
+			if(tmp > count && tmp > 0.0)
+			{
+				count=tmp;
+				recognizeAuthor=author;
+			}
+
+		}
+		return recognizeAuthor;
 	}
 
 	/**
@@ -110,12 +132,37 @@ public class AuthorRecognizer1 extends AuthorRecognizerAbstractClass {
 	public static void main(String[] args) {
 		//initialization of the recognition system
 		AuthorRecognizer1 aut = new AuthorRecognizer1("lm/small_author_corpus/fichConfig_bigram_1000sentences.txt","lm/small_author_corpus/corpus_20000.vocab", "data/author_corpus/validation/authors.txt");
-
 		//computation of the hypothesis author file
-		File sentenceFile = new File("data/small_author_corpus/validation/authors.txt");
-		String sentence = " " + " n ";
-		System.out.println(aut.recognizeAuthorSentence(sentence));
-		
+		String sentence = "J'en ai aucune idée, je me contente de viser dans le trou";
+
+		for(int i=0; i<=1; i++)
+		{
+			String naive = aut.recognizeAuthorSentence(sentence, 0);
+			String laPlace = aut.recognizeAuthorSentence(sentence, 0);
+		//Beginning of the Mindblowing
+			System.out.println("Qui a dit : '" + sentence + "' ?");
+			System.out.println("Oui Naive ?");
+			System.out.println(naive);
+			System.out.println("C'est non, Monsieur Laplace une réponse ?");
+			System.out.println(laPlace);
+
+			if (naive == laPlace && laPlace != "Je ne sais pas" )
+			{
+				System.out.println("Mais enfin Mr Laplace c'est la même réponse que Mr Naive !");
+			}
+			else if(naive == "Je ne sais pas" && laPlace == "Je ne sais pas" && i<1)
+			{
+				System.out.println("Tant pis, QUESTION suivante");
+			}
+			else
+			{
+				System.out.println("C'est un non CA-TE-GO-RIQUE pour les deux joueurs Laplace et Naive");
+			}
+			System.out.println(" ");
+			sentence="et je ne vois pas non plus la vie en rose. ";
+		}
+
+
 		//computation of the performance of the recognition system
 		
 	}
--- src/authorReco/AuthorRecognizerAbstractClass.java
+++ src/authorReco/AuthorRecognizerAbstractClass.java
@@ -109,7 +109,7 @@ public abstract class AuthorRecognizerAbstractClass {
 	 * @param sentence the sentence whose author is to recognize.
 	 * @return the author of the sentence as recognized by the recognition system.
 	 */
-	public abstract String recognizeAuthorSentence(String sentence);
+	public abstract String recognizeAuthorSentence(String sentence, int i);
 
 	/**
 	 * Method processing the sentences of a given reference file (one per line),
@@ -128,7 +128,7 @@ public abstract class AuthorRecognizerAbstractClass {
 
 		int i = 1;
 		for (String sentence: sentences) {
-			authorPredict = recognizeAuthorSentence(sentence);
+			authorPredict = recognizeAuthorSentence(sentence, 0);
 			authorPrediction.append(authorPredict);
 			authorPrediction.append("\n");
 			if(i % 1000 == 0) {
